4d26ba848d202eeceff6f9611ff21947719519c2
Ricardo Santiago <ricardosantiago98@gmail.com>
2023-03-25 00:38:32 -0300

    Add files via upload

---
+++ Vendas2020.sql
@@ -0,0 +1,7 @@
+SELECT [Cliente]
+      ,[ID]
+      ,[Data da Venda]
+
+FROM [Desafio].[dbo].[DB_Teste]
+
+WHERE [Data da Venda] like '%2020%'
---
+++ Vendedor_Equipe.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT [Vendedor],[Equipe]
+
+FROM [Desafio].[dbo].[DB_Teste]
